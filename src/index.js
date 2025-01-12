3e5605ebc227f5d455be0737eb5605b3b9c87f6f
prakharVats <vatsprakhar91@gmail.com>
2025-01-12 14:30:10 +0530

    utils api response and err handling

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@
 // require("dotenv").config({path: "./env"}); one way
 import dotenv from "dotenv" // another way
 import connectDB from "./DB/connect.js";
+import app from "./app.js";
 
 dotenv.config({
     path : "./env"
 })
 
-connectDB();
+connectDB().then(() =>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`listening at PORT : ${process.env.PORT}`);
+    })
+}).catch(err => {
+    console.log("connection failed! " , err)
+});
 
 
 
